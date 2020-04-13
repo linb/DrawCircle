2605c7163ae9da42f9aa9d3b84f5bb46903a5c74
Jack Li <linb.net@gmail.com>
2020-04-13 10:54:51 -0400

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -57,7 +57,7 @@ xui.Class('App', 'xui.Module',{
                 pos = xui.Event.getPos(e),
                 circle = ns.xui_svg_circle12;
             
-            circle.setAttr("KEY",{cx: pos.x, cy: pos.y }, true);
+            circle.setAttr("KEY",{cx: pos.left, cy: pos.top }, true);
         }
     }
 });
