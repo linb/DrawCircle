7ea5a92870ca9118b2e0961cf83e63284c2e68eb
Jack Li <linb.net@gmail.com>
2020-04-13 10:38:33 -0400

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -19,7 +19,15 @@ xui.Class('App', 'xui.Module',{
         iniComponents : function(){
             // [[Code created by CrossUI RAD Studio
             var host=this, children=[], append=function(child){children.push(child.get(0));};
-
+            
+            append(
+                xui.create("xui.UI.SVGPaper")
+                .setHost(host,"xui_ui_svgpaper1")
+                .setDock("fill")
+                .setLeft("9.904761904761905em")
+                .setTop("16.761904761904763em")
+            );
+            
             return children;
             // ]]Code created by CrossUI RAD Studio
         },
